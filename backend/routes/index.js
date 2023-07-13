68a3dd2a526201eeab2bf94a8d312debaea73bd5
ian <iankimmmmm@gmail.com>
2023-07-12 21:25:26 -0700

    Sign up complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,13 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const usersRouter = require('./users/users');
+const { restoreUser } = require("../utils/auth");
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+router.use(restoreUser);
 
 //api
 const apiRouter = require('./api');
@@ -20,5 +27,15 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+router.get("/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
+router.use('/users', usersRouter);
+
 
 module.exports = router;
