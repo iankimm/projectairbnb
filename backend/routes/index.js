79ff963b2aaa77cd235df6032ff08700e5b9e7c9
ian <iankimmmmm@gmail.com>
2023-07-13 19:18:28 -0700

    starting spots

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,6 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-const usersRouter = require('./users/users');
 const { restoreUser } = require("../utils/auth");
 
 // Connect restoreUser middleware to the API router
@@ -14,11 +13,6 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
 
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -27,15 +21,7 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
-router.get("/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
-});
 
-router.use('/users', usersRouter);
 
 
 module.exports = router;
--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -29,6 +29,17 @@ const validateSignup = [
   handleValidationErrors
 ];
 
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
 // Sign up
 router.post(
   '/',
@@ -44,6 +55,44 @@ router.post(
       username: user.username,
       firstName: user.firstName,
       lastName: user.lastName
+    };
+
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
+      user: safeUser
+    });
+  }
+);
+
+//Log in
+router.post(
+  '/login',
+  validateLogin,
+  async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.unscoped().findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential
+        }
+      }
+    });
+
+    if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = { credential: 'The provided credentials were invalid.' };
+      return next(err);
+    }
+
+    const safeUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
     };
 
     await setTokenCookie(res, safeUser);
