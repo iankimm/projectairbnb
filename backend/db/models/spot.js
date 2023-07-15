5a337d0f9f221a50e1d4611918de61c89b5be296
ian <iankimmmmm@gmail.com>
2023-07-15 10:11:01 -0700

    all migrate complete

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -10,7 +10,18 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Spot.hasMany(
+        models.Review,
+        {foreignKey: 'spotId', onDelete: 'CASCADE', hooks: true}
+      ),
+      Spot.hasMany(
+        models.SpotImage,
+        {foreignKey: 'spotId', onDelete: 'CASCADE', hooks: true}
+      ),
+      Spot.belongsTo(
+        models.User,
+        {foreignKey: 'ownerId'}
+      )
     }
   }
   Spot.init({
