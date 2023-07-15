5a7b96cdd4287590af7ae00c3a15c9ccfe59fab1
ian <iankimmmmm@gmail.com>
2023-07-15 13:31:41 -0700

    get reviews complete

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -5,6 +5,21 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
-const { Spot, User } = require('../../db/models');
+const { Spot, User, Review } = require('../../db/models');
 
 const router = express.Router();
+
+//Get all Reviews of the Current User
+router.get('/current', async (req, res) => {
+  const { user } = req;
+
+  let id = user.id;
+
+  let Reviews = await Review.findAll({
+    where: {userId : id }
+  });
+  return res.json({Reviews})
+})
+
+
+module.exports = router;
