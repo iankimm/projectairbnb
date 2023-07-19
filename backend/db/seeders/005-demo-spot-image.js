d5bb8ba2a42fc5f0a0080299562387ba2c05c29b
ian <iankimmmmm@gmail.com>
2023-07-19 09:33:33 -0700

    removed seeders

--- backend/db/seeders/005-demo-spot-image.js
+++ backend/db/seeders/005-demo-spot-image.js
@@ -1,40 +1,40 @@
-'use strict';
+// 'use strict';
 
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
-}
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;  // define your schema in options object
+// }
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    options.tableName = 'SpotImages';
-    return queryInterface.bulkInsert(options, [
-      {
-        spotId: 1,
-        url: "hello url",
-        preview: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 2,
-        url: "hello url",
-        preview: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 3,
-        url: "hello url",
-        preview: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-    ], {});
-  },
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     options.tableName = 'SpotImages';
+//     return queryInterface.bulkInsert(options, [
+//       {
+//         spotId: 1,
+//         url: "hello url",
+//         preview: true,
+//         createdAt: new Date(),
+//         updatedAt: new Date()
+//       },
+//       {
+//         spotId: 2,
+//         url: "hello url",
+//         preview: true,
+//         createdAt: new Date(),
+//         updatedAt: new Date()
+//       },
+//       {
+//         spotId: 3,
+//         url: "hello url",
+//         preview: true,
+//         createdAt: new Date(),
+//         updatedAt: new Date()
+//       },
+//     ], {});
+//   },
 
-  async down (queryInterface, Sequelize) {
-    options.tableName = 'SpotImages';
-    return queryInterface.bulkDelete(options, null, {})
-  }
-};
+//   async down (queryInterface, Sequelize) {
+//     options.tableName = 'SpotImages';
+//     return queryInterface.bulkDelete(options, null, {})
+//   }
+// };
