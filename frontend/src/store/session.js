f2c24f87e67eebe3449a66f5cd322c4ee1613233
ian <iankimmmmm@gmail.com>
2023-08-17 20:18:59 -0700

    signup thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -18,6 +18,7 @@ const removeUser = () => {
 };
 
 //THUNKS
+//LOGIN THUNK
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -39,6 +40,25 @@ export const restoreUser = () => async (dispatch) => {
   return response;
 };
 
+//SIGN UP THUNK
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+//rests
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
