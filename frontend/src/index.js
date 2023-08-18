a4fe8b352610a7c5cb60fa9007fe040ce4d239c5
ian <iankimmmmm@gmail.com>
2023-08-17 19:45:57 -0700

    csrf tested for frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,9 +10,15 @@ import App from './App';
 
 import configureStore from './store';
 
+//CSRF import
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
