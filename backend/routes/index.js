326117aea10fde015acffc9282158a3e6d852b6c
ian <iankimmmmm@gmail.com>
2023-08-18 08:12:12 -0700

    rendering process

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,19 +1,71 @@
+// // backend/routes/index.js
+// const express = require('express');
+// const router = express.Router();
+// const { restoreUser } = require("../utils/auth");
+
+// // Connect restoreUser middleware to the API router
+//   // If current user session is valid, set req.user to the user in the database
+//   // If current user session is not valid, set req.user to null
+// router.use(restoreUser);
+
+// //api
+// const apiRouter = require('./api');
+
+// router.use('/api', apiRouter);
+
+// //FRONT END NEEDED PART!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// // Static routes
+// // Serve React build files in production
+// if (process.env.NODE_ENV === 'production') {
+//   const path = require('path');
+//   // Serve the frontend's index.html file at the root route
+//   router.get('/', (req, res) => {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     return res.sendFile(
+//       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+//     );
+//   });
+
+//   // Serve the static assets in the frontend's build folder
+//   router.use(express.static(path.resolve("../frontend/build")));
+
+//   // Serve the frontend's index.html file at all other routes NOT starting with /api
+//   router.get(/^(?!\/?api).*/, (req, res) => {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     return res.sendFile(
+//       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+//     );
+//   });
+// }
+
+// // Add a XSRF-TOKEN cookie in development
+// if (process.env.NODE_ENV !== 'production') {
+//   router.get('/api/csrf/restore', (req, res) => {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     return res.json({});
+//   });
+// }
+
+// //FRONT END NEEDED PART!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// router.get("/api/csrf/restore", (req, res) => {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({
+//     'XSRF-Token': csrfToken
+//   });
+// });
+
+
+
+// module.exports = router;
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-const { restoreUser } = require("../utils/auth");
-
-// Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
-router.use(restoreUser);
-
-//api
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-//FRONT END NEEDED PART!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // Static routes
 // Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
@@ -21,7 +73,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -32,7 +84,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -42,20 +94,8 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
-
-//FRONT END NEEDED PART!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
-});
-
-
 
 module.exports = router;
