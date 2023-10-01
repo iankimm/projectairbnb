07df3c9c50283126dddc700b70b6f7969dca5040
ian <iankimmmmm@gmail.com>
2023-09-30 17:50:57 -0700

    showing decimals

--- frontend/src/components/SpotIndexItem/index.js
+++ frontend/src/components/SpotIndexItem/index.js
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 const SpotIndexItem = ({spot}) => {
   let averageRating = spot.avgRating;
 
-  if(averageRating == 1) averageRating = 1.0;
-  else if(averageRating == 2) averageRating = 2.0;
-  else if(averageRating == 3) averageRating = 3.0;
-  else if(averageRating == 4) averageRating = 4.0;
-  else if(averageRating == 5) averageRating = 5.0;
+  if(averageRating == 1) averageRating = "1.0";
+  else if(averageRating == 2) averageRating = "2.0";
+  else if(averageRating == 3) averageRating = "3.0";
+  else if(averageRating == 4) averageRating = "4.0";
+  else if(averageRating == 5) averageRating = "5.0";
 
   return (
     <Link className="spotContainers" to={`/spots/${spot.id}`}>
